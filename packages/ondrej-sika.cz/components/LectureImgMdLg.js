8fc40a9ea1e4320a38c0243d5b5fa45ddff1c98e
Ondrej Sika <ondrej@ondrejsika.com>
2019-05-14 22:09:15 +0200

    fix_homepage_course_bar_links

--- packages/ondrej-sika.cz/components/LectureImgMdLg.js
+++ packages/ondrej-sika.cz/components/LectureImgMdLg.js
@@ -5,23 +5,23 @@ const LectureImg = (props) => (
       <div className="d-flex flex-row">
         <div className="box" id='docker'>
           <h4 className='text-center text-white course-header pt-3'><a href='/docker' class='course-header'>DOCKER</a></h4>
-          <a href='/docker'><img src='/static/docker-w.svg' className='img img-fluid p-3'/></a>
+          <a href='/skoleni/docker'><img src='/static/docker-w.svg' className='img img-fluid p-3'/></a>
         </div>
         <div className="box" id='kubernetes'>
           <h4 className='text-center text-white course-header pt-3'><a href='/kubernetes' class='course-header'>KUBERNETES</a></h4>
-          <a href='/kubernetes'><img src='/static/kubernetes.png' className='img img-fluid p-3'/></a>
+          <a href='/skoleni/kubernetes'><img src='/static/kubernetes.png' className='img img-fluid p-3'/></a>
         </div>
         <div className="box" id='git'>
           <h4 className='text-center text-white course-header pt-3'><a href='/git' class='course-header'>GIT</a></h4>
-          <a href='/git'><img src='/static/git-w.svg' className='img img-fluid p-3'></img></a>
+          <a href='/skoleni/git'><img src='/static/git-w.svg' className='img img-fluid p-3'></img></a>
         </div>
         <div className="box" id='gitlab-ci'>
           <h4 className='text-center text-white course-header pt-3'><a href='/gitlab-ci' class='course-header'>GITLAB-CI</a></h4>
-          <a href='/gitlab-ci'><img src='/static/gitlab-ci.svg' className='img img-fluid p-3'></img></a>
+          <a href='/skoleni/gitlab-ci'><img src='/static/gitlab-ci.svg' className='img img-fluid p-3'></img></a>
         </div>
         <div className="box " id='ansible'>
           <h4 className='text-center text-white course-header pt-3'><a href='/ansible' class='course-header'>ANSIBLE</a></h4>
-          <a href='/ansible'><img src='/static/ansible.png' className='img img-fluid p-3'></img></a>
+          <a href='/skoleni/ansible'><img src='/static/ansible.png' className='img img-fluid p-3'></img></a>
         </div>
       </div>
     </div>
