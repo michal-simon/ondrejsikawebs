ee5ab6ad70b43d2e04077cea15c43591b733d7e5
Ondrej Sika <ondrej@ondrejsika.com>
2019-05-14 12:24:04 +0200

    use more markdown

--- packages/ondrej-sika.cz/pages/ansible.js
+++ packages/ondrej-sika.cz/pages/ansible.js
@@ -15,6 +15,10 @@ let site = {
   lang: 'cs'
 }
 
+const H3 = (props) => (
+  <h3 className='pt-4 pb-2'>{props.children}</h3>
+)
+
 const Index = () => (
   <div>
     <Head>
@@ -24,52 +28,51 @@ const Index = () => (
       inquiryBtn='Nezávazně poptat školení'
       logo='/static/ansible.png'>
     </Header>
-    <div className='container'>
-      <TextArea
-      TextHeader='K čemu se používá?'
-      TextParagraph='Ansible je nástroj na hromadnou správu konfiguraci a nasazování. Vy deklarujete vysledný stav serveru a služeb a Ansible se postará o provedení. Nemusíte řešit v jakém stavu je server nebo aplikace ted, Ansible vám zařídí požadovaný výsedek (výhoda deklarativní syntaxe, napřiklad shell script je imperativní - proveď to a to ...). Veškerou konfiguraci máte verzovanou Gitem a nestane se vám ze ztratíte přehled kde máte co nasazené nebo ze vynecháte nějaký důležitý krok. Ansible vám pomůže nasazovat aplikace a spravovat servery rychleji a bezpečněji.'/>
-      <TextArea
-      TextHeader='Jak školení probíhá'
-      TextParagraph='Ansible si společně nainstalujeme a popíšeme si jak pracuje a základní koncepty. Zkusíme si nastavit server (testovací na DigitalOceanu) a nasadit aplikaci od začátku až do konce. Vše si prakticky ošaháme a ozkoušíme.'/>
-      <TextArea
-      TextHeader='Co Vás naučím'
-      >
-      <ReactMarkdown source={`
+    <div className='container course-page'>
+    <ReactMarkdown source={`
+### K čemu se používá?
+
+Ansible je nástroj na hromadnou správu konfiguraci a nasazování. Vy deklarujete vysledný stav serveru a služeb a Ansible se postará o provedení. Nemusíte řešit v jakém stavu je server nebo aplikace ted, Ansible vám zařídí požadovaný výsedek (výhoda deklarativní syntaxe, napřiklad shell script je imperativní - proveď to a to ...). Veškerou konfiguraci máte verzovanou Gitem a nestane se vám ze ztratíte přehled kde máte co nasazené nebo ze vynecháte nějaký důležitý krok. Ansible vám pomůže nasazovat aplikace a spravovat servery rychleji a bezpečněji.
+
+### Jak školení probíhá
+
+Ansible si společně nainstalujeme a popíšeme si jak pracuje a základní koncepty. Zkusíme si nastavit server (testovací na DigitalOceanu) a nasadit aplikaci od začátku až do konce. Vše si prakticky ošaháme a ozkoušíme.
+
+### Co Vás naučím
+
 - Základní koncept Ansible
 - Instalace Ansible (pro ty kteří nenainstalovali doma)
 - Základní ovládání Ansible
 - Playbooks
 - Moduly
 - Šablony
-- Secrets`}/>
-      </TextArea>
+- Secrets
 
-    <TextArea
-      TextHeader='Pro koho je školení určeno'
-      TextParagraph='Školení je převážně určeno adminum a DevOps, kteří chtějí začít Ansible používat. Zároveň je vhodné i pro vývojáře, kteří občas nasazují (i do testovacích prostředí) svoje aplikace a chtějí zjednodušit tento proces.'>
-      <TwoCol
-        col1={
-          <>
-            <h4>Předchozí znalosti</h4>
-            <ReactMarkdown source={`
+### Pro koho je školení určeno
+
+Školení je převážně určeno adminum a DevOps, kteří chtějí začít Ansible používat. Zároveň je vhodné i pro vývojáře, kteří občas nasazují (i do testovacích prostředí) svoje aplikace a chtějí zjednodušit tento proces.
+`}/>
+
+  <TwoCol
+    col1={
+      <ReactMarkdown source={`
+#### Předchozí znalosti
+
 - Základy práce s Linuxemm
 - Základy práce v terminálu
-            `} escapeHtml={false}/>
-          </>
-        }
-        col2={
-          <>
-            <h4>Technické požadavky</h4>
-            <ReactMarkdown source={`
+      `} escapeHtml={false}/>
+    }
+    col2={
+      <ReactMarkdown source={`
+#### Technické požadavky
+
 - Nainstalovaný Ansible
 - Přístup na internet (ideálně bez korporatni proxy, případně nastavenou proxy v Dockeru)
-            `} escapeHtml={false}/>
-          </>
-        }
-      />
-    </TextArea>
+      `} escapeHtml={false}/>
+    }
+  />
 
-    </div>
+  </div>
     <References
       NameRef1='Adrian Kantor'
       PositionRef1='Head of IT Prague Fio banka'
