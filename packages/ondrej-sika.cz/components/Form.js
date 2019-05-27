82ba72f2360bb50d6c13b247b0687a08275a6965
Ondrej Sika <ondrej@ondrejsika.com>
2019-05-27 14:58:57 +0200

    iiiii

--- packages/ondrej-sika.cz/components/Form.js
+++ packages/ondrej-sika.cz/components/Form.js
@@ -7,19 +7,19 @@ const Form = (props) => (
       <input type="hidden" value={props.course} name="course"/>
       <div className="form-group">
         <div className="col-md-4 mb-3 pl-0">
-          <label for="validationDefault01">{props.FormName}</label>
+          <label htmlFor="validationDefault01">{props.FormName}</label>
           <input type="text" className="form-control-border" id="validationDefault01" name="name" placeholder={props.FromNamePlaceholder} required />
         </div>
         <div className="col-md-4 mb-3 pl-0">
-          <label for="validationDefault02">{props.FormCompany}Společnost</label>
+          <label htmlFor="validationDefault02">{props.FormCompany}Společnost</label>
           <input type="text" className="form-control-border" id="validationDefault02" name="company" placeholder={props.FormCompanyPlaceholder} />
         </div>
         <div className="col-md-4 mb-3 pl-0">
-          <label for="validationDefault03">{props.FormEmail}</label>
+          <label htmlFor="validationDefault03">{props.FormEmail}</label>
           <input type="email" className="form-control-border" id="validationDefault03" name="email" placeholder={props.FormEmailPlaceholder} required />
         </div>
         <div className="col-md-4 mb-3 pl-0">
-          <label for="validationDefault04">{props.FormPhone}</label>
+          <label htmlFor="validationDefault04">{props.FormPhone}</label>
           <input type="tel" className="form-control-border" id="validationDefault04" name="phone" placeholder={props.FormPhonePlaceholder} required />
         </div>
       </div>
