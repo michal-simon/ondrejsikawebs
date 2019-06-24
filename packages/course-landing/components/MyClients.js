379e7362cd79f151ac10a6830180af6ea78fdc70
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-24 18:21:14 +0200

    skolenie-kubernetes.sk - images moved to data, but it does not work

--- packages/course-landing/components/MyClients.js
+++ packages/course-landing/components/MyClients.js
@@ -1,10 +1,3 @@
-import termo_fish from './MyClients/thermo_fish.jpg'
-import aaa_auto from './MyClients/aaa_auto.png'
-import kb from './MyClients/kb.png'
-import fio from './MyClients/fio.png'
-import o2 from './MyClients/o2.png'
-import ceska_pojistovna from './MyClients/ceska_pojistovna.jpg'
-
 import Translate from '@app/common/components/Translate'
 
 const MyClients = (props) => (
@@ -20,22 +13,22 @@ const MyClients = (props) => (
       </h3>
       <div className="d-flex row text-center pt-3 pb-3">
         <div className="col-lg-2 col-md-4 col-sm-4 col-6">
-          <img src={termo_fish} className="img-fluid" alt="default" />
+          <img src={props.image1} className="img-fluid" />
         </div>
         <div className="col-lg-2 col-md-4 col-sm-4 col-6">
-          <img src={aaa_auto} className="img-fluid" alt="default" />
+          <img src={props.image2} className="img-fluid" />
         </div>
         <div className="col-lg-2 col-md-4 col-sm-4 col-6">
-          <img src={kb} className="img-fluid" alt="default" />
+          <img src={props.image3} className="img-fluid" />
         </div>
         <div className="col-lg-2 col-md-4 col-sm-4 col-6">
-          <img src={fio} className="img-fluid" alt="default" />
+          <img src={props.image4} className="img-fluid" />
         </div>
         <div className="col-lg-2 col-md-4 col-sm-4 col-6">
-          <img src={o2} className="img-fluid" alt="default" />
+          <img src={props.image5} className="img-fluid" />
         </div>
         <div className="col-lg-2 col-md-4 col-sm-4 col-6">
-          <img src={ceska_pojistovna} className="img-fluid" alt="default" />
+          <img src={props.image6}  className="img-fluid" />
         </div>
       </div>
     </div>
